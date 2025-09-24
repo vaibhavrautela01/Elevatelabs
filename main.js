475dce42ffd69241c14326363f8ac2dc6b8a4725
Vaibhav Rautela <rautelavaibhav01@gmail.com>
2025-09-24 14:38:47 +0530

    Test CI/CD trigger

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = 3000;
 
 app.get("/", (req, res) => {
-  res.send("Hello From Vaibhav Rautela this is CI-CD Node");
+  res.send("Hello From Vaibhav Rautela");
 });
 
 app.listen(PORT, () => {
