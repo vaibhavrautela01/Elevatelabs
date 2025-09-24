1232c516991d54b52b99a973dee945bb79cd075d
Vaibhav Rautela <rautelavaibhav01@gmail.com>
2025-09-24 14:19:03 +0530

    First commit - pushing project files

---
+++ main.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+const PORT = 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hello From Vaibhav Rautela this is CI-CD Node");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port: http://localhost:${PORT}`);
+});
